ad0dad64667adbae2d62f614d5e34e1179d24c66
awahiid <awmahfoud@proton.me>
2026-05-02 03:17:22 +0200

    feat(ui): update footer links to include target and href attributes for GitHub and LinkedIn

--- frontend/src/components/ui/Card.tsx
+++ frontend/src/components/ui/Card.tsx
@@ -1,6 +1,9 @@
-export function Card({ children }: { children: React.ReactNode }) {
+import React from "react";
+import {cn} from "@/utils/cn";
+
+export function Card({ children, className }: { children: React.ReactNode, className?: string }) {
     return (
-        <div className={'relative bg-gradient-to-b from-white via-white to-white/90 sm:my-40 m-auto sm:w-2/6 z-10 gap-10 flex border border-white sm:shadow-2xl sm:shadow-neutral flex-col p-10'}>
+        <div className={cn(className, 'justify-start sm:h-auto relative bg-gradient-to-b from-white via-white to-white/90 sm:my-40 m-auto sm:w-2/6 z-10 gap-10 flex border border-white sm:shadow-2xl sm:shadow-neutral flex-col p-10')}>
             {children}
         </div>
     )
--- frontend/src/components/ui/GeneralBackground.tsx
+++ frontend/src/components/ui/GeneralBackground.tsx
@@ -1,12 +1,13 @@
 import React from "react";
+import {cn} from "@/utils/cn";
 
-export function GeneralBackground({children}: { children: React.ReactNode }) {
+export function GeneralBackground({children, className}: { children: React.ReactNode, className?: string }) {
     return (
         <div className={'w-full bg-neutral relative'}>
             <div className={'absolute inset-0 pattern-grid-white/70 pattern-size-40 pattern-size-30 pattern-thickness-2 pattern-offset-24'}></div>
             <div className={'absolute inset-0 bg-gradient-to-b from-neutral to-transparent'}></div>
             <div className={'max-w-7xl relative mx-auto border border-transparent h-full'}>
-                <div className={'max-w-7xl relative mx-auto border border-transparent h-full'}>
+                <div className={cn(className, 'max-w-7xl relative mx-auto border border-transparent h-full')}>
                     {children}
                 </div>
             </div>
